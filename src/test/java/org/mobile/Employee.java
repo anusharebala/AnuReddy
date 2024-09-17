5e1883a2dc7176dbe03c136bab37ff0cbbfba951
anusharebala <anusharebala@gmail.com>
2024-09-17 10:40:35 +0530

    modified

--- src/test/java/org/mobile/Employee.java
+++ src/test/java/org/mobile/Employee.java
@@ -12,4 +12,16 @@ public class Employee {
 	private void method3() {
 		System.out.println(3);
 	}
+	private void method7() {
+		System.out.println(1);
+
+	}
+	private void method8() {
+		System.out.println(2);
+
+	}
+	private void method9() {
+		System.out.println(3);
+	}
 }
+
